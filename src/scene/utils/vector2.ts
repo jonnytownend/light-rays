0bf9dc4adde967478b724b48217bf43b96aa1279
Jonny Townend <jonny@brightec.co.uk>
2020-12-29 22:18:16 +0000

    Implement Vector2 and add empty export lines to empty ts files

---
+++ src/scene/utils/vector2.ts
@@ -0,0 +1,88 @@
+class Vector2 {
+    x: number
+    y: number
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    len() {
+        const sqr = (this.x*this.x) + (this.y*this.y);
+        return Math.sqrt( sqr );
+    }
+
+    add( vect: Vector2 ) {
+        const x = this.x + vect.x;
+        const y = this.y + vect.y;
+
+        return new Vector2(x,y);
+    }
+
+    subtract( vect: Vector2 ) {
+        const x = this.x - vect.x;
+        const y = this.y - vect.y;
+
+        return new Vector2(x,y);
+    }
+
+    scalarMult( scalar: number ) {
+        const x = this.x*scalar;
+        const y = this.y*scalar;
+
+        return new Vector2(x,y);
+    }
+
+    scale( scale: number ) {
+        const x = this.x*scale/this.len();
+        const y = this.y*scale/this.len();
+        this.x = x;
+        this.y = y;
+    }
+
+    rotate( angle: number ) {
+        const x = this.x*Math.cos(angle) - this.y*Math.sin(angle);
+        const y = this.x*Math.sin(angle) + this.y*Math.cos(angle);
+        this.x = x;
+        this.y = y;
+    }
+
+    dot( vect: Vector2 ) {
+        return this.x*vect.x + this.y*vect.y;
+    }
+
+    angleBetween( vect: Vector2 ) {
+        const dot = this.dot( vect );
+        const mult = this.len() * vect.len();
+        if (mult==0) {
+            return false;
+        }
+        else {
+            const costheta = dot/mult;
+            return Math.acos( costheta );
+        }
+    }
+
+    normal() {
+        return new Vector2(this.y, -this.x);
+    }
+
+    set(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    copy() {
+        return new Vector2(this.x, this.y);
+    }
+
+    drawFrom(context: CanvasRenderingContext2D, x: number, y: number) {
+        context.beginPath();
+        context.moveTo( x, y );
+        context.lineTo( x + this.x, y + this.y );
+        context.stroke();
+        context.closePath();
+    }
+}
+
+export default Vector2
